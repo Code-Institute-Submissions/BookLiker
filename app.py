fa73b6ccc0daaf70aacb23a3ac2cc5c337b71a03
varroz56 <zoltanlvarro@gmail.com>
2020-05-02 10:15:17 +0000

    installed pymongo, connected app to the db, created basic template and tested connection with find query

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 
 
+app.config["MONGO_URI"]= os.environ.get('MONGO_URI')
+
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World and Flask"
+@app.route('/get_books')
+def get_books():
+    return render_template("books.html", books=mongo.db.Books.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
