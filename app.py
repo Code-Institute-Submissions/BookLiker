b594e646d9de9f91e6a218ce8952085158befda9
varroz56 <zoltanlvarro@gmail.com>
2020-05-02 12:22:08 +0000

    created aboutbook.html, wired up to more info using objectid

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -15,6 +16,13 @@ mongo = PyMongo(app)
 @app.route('/get_books')
 def get_books():
     return render_template("books.html", books=mongo.db.Books.find())
+
+
+@app.route('/about_book/<book_id>')
+def about_book(book_id):
+    return render_template("aboutbook.html", 
+    book=mongo.db.Books.find_one({"_id": ObjectId(book_id)}))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
